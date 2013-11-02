5b0c218968952c43adb017eba41b0c3ad5bcdbdf
xenobyte <timo.brd@gmx.net>
2013-11-02 09:56:08 +0000

    Initial

---
+++ src/funktionen.py
@@ -0,0 +1,16 @@
+#
+# BEGRUESSUNG
+#
+
+
+def begruessung(name):
+    return "Willkommen " + name
+
+name = eval(input("Wie ist dein werter Name?: "))
+
+text = begruessung(name)
+
+print(text)
+
+
+
